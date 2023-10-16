85baa92c28e183333f3cb07661124945482e40d4
Gurparshad Sharma <gurparshad.sharma@visioncraft.ee>
2023-10-16 10:54:42 +0300

    frontend changes

---
+++ client/src/pages/dashboard/Row2.tsx
@@ -0,0 +1,14 @@
+import DashboardBox from "@/components/styled-components/DashboardBox";
+import React from "react";
+
+const Row1 = () => {
+  return (
+    <>
+      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
---
+++ client/src/pages/dashboard/Row3.tsx
@@ -0,0 +1,14 @@
+import DashboardBox from "@/components/styled-components/DashboardBox";
+
+const Row1 = () => {
+  return (
+    <>
+      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
